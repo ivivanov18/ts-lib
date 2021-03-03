a80206b9a45f8f6479a22718c4dec1f9402bbf5b
Ivan Ivanov <bolzano@DESKTOP-IT80QOV.localdomain>
2021-03-03 21:03:59 +0200

    add api-extractor - add package - add basic setup

---
+++ src/substract.ts
@@ -0,0 +1,11 @@
+/**
+ * Calculate the difference between 2 numbers
+ *
+ * @param a - first number
+ * @param b - second number
+ *
+ * @beta
+ */
+export function substract(a: number, b: number) {
+  return a - b;
+}
